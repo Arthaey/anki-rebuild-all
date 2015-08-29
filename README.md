f4d43942f3e351b937b84eb70704c32507efa42a
Arthaey <arthaey@gmail.com>
2015-08-28 19:58:59 -0500

    Adds a "Rebuild All" button to the bottom of the main screen.

---
+++ README.md
@@ -0,0 +1,21 @@
+# Description
+
+An Anki add-on that adds a "Rebuild All" button to the bottom of the main screen,
+to rebuild all filtered decks at once.
+
+# Support
+
+Post a [new issue on Github](https://github.com/Arthaey/anki-rebuild-all/issues/new)
+(or make a pull request!).
+
+[My other Anki add-ons](https://github.com/search?q=user%3AArthaey+anki)
+are also on Github.
+
+# TODO
+
+- add config for individual decks to "opt out" of rebuild-all
+
+# License
+
+This addon is licensed under the same license as Anki itself (GNU Affero General
+Public License 3).
---
+++ RebuildAll.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+# See github page to report issues or to contribute:
+# https://github.com/Arthaey/anki-rebuild-all
+
+import re
+from anki.hooks import wrap
+from aqt.deckbrowser import DeckBrowser
+
+def _rebuildAll(self, url):
+    if url == "rebuild":
+        mw = self.mw
+        dynDeckIds = [ d["id"] for d in mw.col.decks.all() if d["dyn"] ]
+        mw.checkpoint("rebuild {0} decks".format(len(dynDeckIds)))
+        mw.progress.start()
+        [ mw.col.sched.rebuildDyn(did) for did in dynDeckIds ]
+        mw.progress.finish()
+        mw.reset()
+
+def _addButton(self):
+    # There's no clean way to add a button, so hack it in. :(
+    button = "<button title='Rebuild All' onclick='py.link(\"rebuild\");'>Rebuild All</button>"
+    html = self.bottom.web.page().mainFrame().toHtml()
+    html = re.sub("</button></td>", u"</button>{0}</td>".format(button), html)
+    self.bottom.draw(html)
+
+DeckBrowser._drawButtons = wrap(DeckBrowser._drawButtons, _addButton, "after")
+DeckBrowser._linkHandler = wrap(DeckBrowser._linkHandler, _rebuildAll, "after")
