eff9d2a505fc6b3d86dcd2007d8443035bedf097
Arthaey <arthaey@gmail.com>
2017-07-12 23:29:27 -0700

    Added "Empty All" button per PR #2 by @ankitest, inspired by feature request #1 from @DRroscoe. Thanks! :)

--- RebuildAll.py
+++ RebuildAll.py
@@ -3,27 +3,57 @@
 # https://github.com/Arthaey/anki-rebuild-all
 #
 # Also available for Anki at https://ankiweb.net/shared/info/1639597619
+#
+# Contributors:
+# - @Arthaey
+# - @ankitest
 
 import re
 from anki.hooks import wrap
+from aqt import mw
 from aqt.deckbrowser import DeckBrowser
+from aqt.utils import tooltip
 
-def _rebuildAll(self, url):
-    if url == "rebuild":
-        mw = self.mw
-        dynDeckIds = [ d["id"] for d in mw.col.decks.all() if d["dyn"] ]
-        mw.checkpoint("rebuild {0} decks".format(len(dynDeckIds)))
-        mw.progress.start()
-        [ mw.col.sched.rebuildDyn(did) for did in sorted(dynDeckIds) ]
-        mw.progress.finish()
-        mw.reset()
 
-def _addButton(self):
+def _updateFilteredDecks(actionFuncName):
+    dynDeckIds = [ d["id"] for d in mw.col.decks.all() if d["dyn"] ]
+    count = len(dynDeckIds)
+
+    if not count:
+        tooltip("No filtered decks found.")
+        return
+
+    # should be one of "rebuildDyn" or "emptyDyn"
+    actionFunc = getattr(mw.col.sched, actionFuncName)
+
+    mw.checkpoint("{0} {1} filtered decks".format(actionFuncName, count))
+    mw.progress.start()
+    [ actionFunc(did) for did in sorted(dynDeckIds) ]
+    mw.progress.finish()
+    tooltip("Updated {0} filtered decks.".format(count))
+
+    mw.reset()
+
+
+def _handleFilteredDeckButtons(self, url):
+    _updateFilteredDecks(url)
+
+
+def _addButtons(self):
     # There's no clean way to add a button, so hack it in. :(
-    button = "<button title='Rebuild All' onclick='py.link(\"rebuild\");'>Rebuild All</button>"
+    newHtml = ""
+    newButtons = [
+        [_("Rebuild All"), "rebuildDyn"],
+        [_("Empty All"),   "emptyDyn"],
+    ]
+
+    for newButton in newButtons:
+        newHtml += "<button title='{0}' onclick='py.link(\"{1}\");'>{0}</button>".format(*newButton)
+
     html = self.bottom.web.page().mainFrame().toHtml()
-    html = re.sub("</button></td>", u"</button>{0}</td>".format(button), html)
+    html = re.sub("</button></td>", u"</button>{0}</td>".format(newHtml), html)
     self.bottom.draw(html)
 
-DeckBrowser._drawButtons = wrap(DeckBrowser._drawButtons, _addButton, "after")
-DeckBrowser._linkHandler = wrap(DeckBrowser._linkHandler, _rebuildAll, "after")
+
+DeckBrowser._drawButtons = wrap(DeckBrowser._drawButtons, _addButtons, "after")
+DeckBrowser._linkHandler = wrap(DeckBrowser._linkHandler, _handleFilteredDeckButtons, "after")
